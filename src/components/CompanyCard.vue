d118bbd720b2d0954db6d70aea586affe0a356ef
Nand Kishor <nandkk07@gmail.com>
2021-05-19 12:04:32 +0530

    skeleton loading

--- src/components/CompanyCard.vue
+++ src/components/CompanyCard.vue
@@ -1,33 +1,37 @@
-<template>
-  <div class="xl:w-1/3 md:w-1/2 p-3 sm: w-full">
+<template >
+  <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full">
     <div
-      class="shadow-xl p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full bg-gray-900 border-gray-800"
+      class="anime shadow-xl p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full bg-gray-900 border-gray-800"
     >
       <div class="h-full flex items-center p-4 rounded-lg">
         <img
           alt="team"
-          class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+          class="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4"
           :src="
             jobs.company_logo != null
               ? jobs.company_logo
               : 'https://logodix.com/logo/32094.jpg'
           "
         />
-        <div class="p-3">
+        <div class="sm:p-4">
           <div
-            class="w-max text-xs leading-wide font-semibold text-indigo-800 bg-indigo-200 px-2 rounded-full"
+            class="w-max text-xs leading-wide font-semibold text-indigo-800 bg-indigo-200 px-2 sm:py-0.5 rounded-full"
           >
-            {{ jobs.type }}
+            {{ jobs.type }} - {{ jobs.location }}
           </div>
-          <h4 class="font-semibold text-xl mt-2 truncate">
+          <div
+            class="font-medium lg: font-semibold sm:text-xl mt-3 overflow-hidden ..."
+          >
             {{ jobs.title }}
-          </h4>
-          <div class="mt-1 leading-relaxed truncate text-gray-400">
+          </div>
+          <div
+            class="mt-1 leading-relaxed sm: text-xs overflow-ellipsis overflow-hidden ... text-gray-400 sm:text-sm"
+          >
             {{ jobs.company }}
           </div>
           <div>
             <a
-              class="mt-2 inline-flex items-center font-semibold text-indigo-400"
+              class="mt-4 inline-flex items-center font-semibold text-indigo-400"
               :href="jobs.url"
               >See job
               <svg
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -31,7 +31,7 @@
           devJobs
         </h1>
         <p
-          class="lg:w-2/3 mx-auto leading-relaxed font-semibold text-lg text-gray-300"
+          class="lg:w-2/3 mx-auto leading-relaxed sm:font-semibold sm:text-lg text-gray-300"
         >
           Platform for searching your next dream job &#128640;
         </p>
@@ -43,13 +43,265 @@
           <input
             type="text"
             v-model="search"
-            class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            class="w-full bg-gray-800 bg-opacity-40 rounded-lg border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
             placeholder="Filter by title, company name..."
           />
         </div>
       </div>
       <div class="p-4 flex flex-wrap items-center justify-left bg-gray-800">
         <CompanyCard :jobs="job" v-for="job in filteredJobs" :key="job.id" />
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="xl:w-1/3 md:w-1/2 sm:p-3 sm: w-full" v-if="loading">
+          <div
+            class="shadow-xl bg-gray-900 border-gray-800 p-2 flex flex-wrap rounded-lg overflow-hidden sm: mt-4 w-full"
+          >
+            <div class="flex items-center p-4 rounded-lg">
+              <div
+                class="w-16 h-16 bg-gray-700 border-gray-700 rounded-full mr-4 animate-pulse"
+              ></div>
+              <div class="sm:p-6 w-max">
+                <div
+                  class="mt-3 w-full bg-gray-700 text-gray-700 text-sm rounded animate-pulse"
+                >
+                  loading loading data ...........
+                </div>
+                <div
+                  class="w-20 mt-1 bg-gray-700 text-gray-700 py-1 text-sm rounded animate-pulse"
+                ></div>
+                <div>
+                  <a
+                    class="mt-4 inline-flex items-center bg-indigo-900 text-indigo-900 rounded-lg animate-pulse"
+                  >
+                    loading data
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -66,15 +318,18 @@ export default {
     search: "",
     checkbox: false,
     jobs: [],
+    loading: true,
   }),
 
   methods: {
-    fetchApi() {
-      axios
-        .get("http://localhost:8080/positions.json")
-        //.then((response) => (this.jobs = response.data))
-        .then((response) => console.log(response))
+    async fetchApi() {
+      await axios
+        .get(
+          "https://mycorsproxy005.herokuapp.com/https://jobs.github.com//positions.json"
+        )
+        .then((response) => (this.jobs = response.data))
         .catch((e) => console.log(e));
+      this.loading = false;
     },
   },
   mounted() {
